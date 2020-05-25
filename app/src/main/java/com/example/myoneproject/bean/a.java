a156220f484f0e8fca909117284528f8a79003c1
ysp <ysp>
2020-05-25 16:44:43 +0800

    新建了三个文件

---
+++ app/src/main/java/com/example/myoneproject/bean/a.java
@@ -0,0 +1,9 @@
+package com.example.myoneproject.bean;
+
+/**
+ * Created by :YuShuangPing
+ * Time:2020/5/25 15:07
+ * Describe:
+ */
+public class a {
+}
---
+++ app/src/main/java/com/example/myoneproject/bean/c.java
@@ -0,0 +1,9 @@
+package com.example.myoneproject.bean;
+
+/**
+ * Created by :YuShuangPing
+ * Time:2020/5/25 15:08
+ * Describe:
+ */
+public class c {
+}
